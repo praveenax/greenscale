dff4300bf0afb9351e37785b3c1f063fa92f19c6
praveenax <xav.axp@gmail.com>
2014-11-08 19:54:10 +0530

    With sliding menu

---
+++ GScale/src/com/gs/gscale/PhotosFragment.java
@@ -0,0 +1,22 @@
+package com.gs.gscale;
+
+import info.androidhive.slidingmenu.R;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PhotosFragment extends Fragment {
+	
+	public PhotosFragment(){}
+	
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+ 
+        View rootView = inflater.inflate(R.layout.fragment_photos, container, false);
+         
+        return rootView;
+    }
+}
