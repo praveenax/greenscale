020feaf8b5ce18303c2a280c4a38c60355c7041d
Vasanth <vasanthps@live.com>
2014-11-08 19:26:12 +0530

    added ab access

---
+++ GreenScale/src/dblib/CarbonFootprintQuickLoadData.java
@@ -0,0 +1,9 @@
+package dblib;
+
+public class CarbonFootprintQuickLoadData {
+	
+	public static java.util.Date date= null;
+	public static double totalFootprint = 0;
+	public static CarbonFootprintData lastActivity = null;
+	
+}
---
+++ GreenScale/src/dblib/DBAccess.java
@@ -0,0 +1,11 @@
+package dblib;
+
+public class DBAccess {
+	public DBAccess(String path) {
+		this.dbPath = path;
+	}
+	
+	String dbPath = "";
+	
+	
+}
